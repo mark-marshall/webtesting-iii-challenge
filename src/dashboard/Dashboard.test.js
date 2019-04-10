29df0979b86d2a7967e247eb3aa4446e584fd948
mark-marshall <mark.marshallgp@gmail.com>
2019-04-10 14:50:02 +0100

    Added snapshots from Dashboard component

--- src/dashboard/Dashboard.test.js
+++ src/dashboard/Dashboard.test.js
@@ -13,9 +13,11 @@ describe('<Dashboard />', () => {
     it('renders the display', () => {
         const wrap = rt.render(<Dashboard />);
         expect(wrap.getByTestId(/displayDiv/i));
+        expect(wrap.asFragment()).toMatchSnapshot();
     })
     it('renders the controls', () => {
         const wrap = rt.render(<Dashboard />);
         expect(wrap.getByTestId(/displayDiv/i));
+        expect(wrap.asFragment()).toMatchSnapshot();
     })
 })
