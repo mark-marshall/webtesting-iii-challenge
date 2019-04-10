16e0181cef0d621d743e4a751ae41ab5c2f9f5e1
mark-marshall <mark.marshallgp@gmail.com>
2019-04-10 14:12:03 +0100

    [MVP] Added tests for dynamic class-names on the Display component

--- src/display/Display.test.js
+++ src/display/Display.test.js
@@ -3,11 +3,12 @@
 // - displays if gate is open/closed and if it is locked/unlocked
 // - displays 'Closed' if the `closed` prop is `true` and 'Open' if otherwise (Done)
 // - displays 'Locked' if the `locked` prop is `true` and 'Unlocked' if otherwise (Done)
-// - when `locked` or `closed` use the `red-led` class
-// - when `unlocked` or `open` use the `green-led` class
+// - when `locked` or `closed` use the `red-led` class (Done)
+// - when `unlocked` or `open` use the `green-led` class (Done)
 
 import React from 'react';
 import * as rt from 'react-testing-library';
+import 'jest-dom/extend-expect';
 
 import Display from './Display';
 
@@ -38,16 +39,20 @@ describe('<Display />', () => {
     const wrap = rt.render(<Display locked={0} />);
     expect(wrap.getAllByText(/unlocked/i));
   });
-//   it('uses the red-led class when locked', () => {
-//     const wrap = rt.render(<Display locked={true}/>);
-//   });
-//   it('uses the red-led class when closed', () => {
-//     const wrap = rt.render(<Display closed={true}/>);
-//   });
-//   it('uses the green-led class when unlocked', () => {
-//     const wrap = rt.render(<Display locked={false}/>);
-//   });
-//   it('uses the green-led class when open', () => {
-//     const wrap = rt.render(<Display closed={false}/>);
-//   });
+  it('uses the red-led class when locked', () => {
+    const wrap = rt.render(<Display locked={true}/>);
+    expect(wrap.getByTestId(/lockedDiv/i)).toHaveClass('red-led');
+  });
+  it('uses the red-led class when closed', () => {
+    const wrap = rt.render(<Display closed={true}/>);
+    expect(wrap.getByTestId(/closedDiv/i)).toHaveClass('red-led');
+  });
+  it('uses the green-led class when unlocked', () => {
+    const wrap = rt.render(<Display locked={false}/>);
+    expect(wrap.getByTestId(/lockedDiv/i)).toHaveClass('green-led');
+  });
+  it('uses the green-led class when open', () => {
+    const wrap = rt.render(<Display closed={false}/>);
+    expect(wrap.getByTestId(/closedDiv/i)).toHaveClass('green-led');
+  });
 });
