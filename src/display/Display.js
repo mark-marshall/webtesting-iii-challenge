b7d848eeb87f47c4fd5e4e8eeae057c6211bccc3
mark-marshall <mark.marshallgp@gmail.com>
2019-04-10 13:22:58 +0100

    [MVP] Added tests for prop-variation on Display component

--- src/display/Display.js
+++ src/display/Display.js
@@ -6,8 +6,8 @@ const Display = ({ closed, locked }) => {
 
   return (
     <div className="display panel">
-      <div className={lockedClass}>{locked ? 'Locked' : 'Unlocked'}</div>
-      <div className={closedClass}>{closed ? 'Closed' : 'Open'}</div>
+      <div className={lockedClass} data-testid='lockedDiv'>{locked ? 'Locked' : 'Unlocked'}</div>
+      <div className={closedClass} data-testid='closedDiv'>{closed ? 'Closed' : 'Open'}</div>
     </div>
   );
 };
---
+++ src/display/Display.test.js
@@ -0,0 +1,41 @@
+// ### Display Component
+
+// - displays if gate is open/closed and if it is locked/unlocked
+// - displays 'Closed' if the `closed` prop is `true` and 'Open' if otherwise (Done)
+// - displays 'Locked' if the `locked` prop is `true` and 'Unlocked' if otherwise (Done)
+// - when `locked` or `closed` use the `red-led` class
+// - when `unlocked` or `open` use the `green-led` class
+
+import React from 'react';
+import * as rt from 'react-testing-library';
+
+import Display from './Display';
+
+afterEach(rt.cleanup);
+
+describe('<Display />', () => {
+  it('displays closed when the closed prop is true', () => {
+    const wrap = rt.render(<Display closed={true} />);
+    expect(wrap.getAllByText(/closed/i));
+  });
+  it('displays open when the closed prop is false', () => {
+    const wrap = rt.render(<Display closed={false} />);
+    expect(wrap.getAllByText(/open/i));
+  });
+  it('displays open when the closed prop is falsy', () => {
+    const wrap = rt.render(<Display closed={0} />);
+    expect(wrap.getAllByText(/open/i));
+  });
+  it('displays locked when the locked prop is true', () => {
+    const wrap = rt.render(<Display locked={true} />);
+    expect(wrap.getAllByText(/locked/i));
+  });
+  it('displays unlocked when the locked prop is false', () => {
+    const wrap = rt.render(<Display locked={false} />);
+    expect(wrap.getAllByText(/unlocked/i));
+  });
+  it('displays unlocked when the locked prop is falsy', () => {
+    const wrap = rt.render(<Display locked={0} />);
+    expect(wrap.getAllByText(/unlocked/i));
+  });
+});
